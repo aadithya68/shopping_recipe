0aebe594e2ed5bb57a0b0ec4255bb6db9488c530
Aadithya S <aadits12@in.ibm.com>
2018-10-15 19:34:58 +0530

    Header module

--- src/app/header/header.module.ts
+++ src/app/header/header.module.ts
@@ -1,17 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { HeaderComponent } from './header.component';
+// import { HeaderComponent } from './header.component';
 
 @NgModule({
     declarations: [
-      HeaderComponent,
+      // HeaderComponent,
     ],
     imports: [
       BrowserModule
     ],
     providers: [],
-    bootstrap: [HeaderComponent]
+    bootstrap: []
   })
 
   export class HeaderModule {}
